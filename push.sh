5c05160ff09cbb0aca8e155f741f65b0638b825a
Slawomir Koszewski <slawek@koszewscy.waw.pl>
2025-09-27 14:10:25 +0200

    Update repository URL in push script to use docker.io

--- push.sh
+++ push.sh
@@ -3,5 +3,7 @@
 # Set the image name to the current directory name if not provided
 IMAGE_NAME="${IMAGE_NAME:-$(basename $(pwd))}"
 
-docker tag "$IMAGE_NAME" "skdomlab.azurecr.io/$IMAGE_NAME:latest"
-docker push "skdomlab.azurecr.io/$IMAGE_NAME:latest"
+REPOSITORY="docker.io/skoszewski"
+
+docker tag "$IMAGE_NAME" "$REPOSITORY/$IMAGE_NAME:latest"
+docker push "$REPOSITORY/$IMAGE_NAME:latest"

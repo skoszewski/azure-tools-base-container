cbd9f5068dfca6209420659b4d51a214024965af
Sławomir Koszewski <slawomir.koszewski@medicover.com>
2025-09-29 20:20:49 +0200

    Refactor Build.ps1 to improve environment variable handling and architecture tagging

--- Build.ps1
+++ Build.ps1
@@ -1,7 +1,40 @@
 [CmdletBinding()]
 param (
-    [switch]$Push = $false
+    [switch]$Push = $false,
+    [ValidateSet("amd64", "arm64")]
+    [string]$Architecture = "amd64"
 )
+
+function Import-EnvFile {
+    [CmdletBinding()]
+    param (
+        [string]$EnvFilePath    
+    )
+    
+    Get-Content -Path $EnvFilePath | ForEach-Object {
+        if ($_ -match '^\s*#') {
+            # Skip comment lines
+            return
+        }
+
+        if ($_ -match '^\s*$') {
+            # Skip empty lines
+            return
+        }
+
+        $parts = $_ -split '=', 2
+    
+        if ($parts.Length -eq 2) {
+            $key = $parts[0].Trim()
+            $value = $parts[1].Trim()
+            [System.Environment]::SetEnvironmentVariable($key, $value)
+            Write-Verbose "Set environment variable: $key"
+        }
+        else {
+            Write-Warning "Ignoring invalid line in env file: $_"
+        }
+    }
+}
 
 # Check, if build_env.ps1 exists
 if (-not (Test-Path -Path "$PSScriptRoot\build_env.ps1")) {
@@ -12,20 +45,20 @@ if (-not (Test-Path -Path "$PSScriptRoot\build_env.ps1")) {
 Write-Verbose "Loading build environment from `"$PSScriptRoot\build_env.ps1`""
 
 # Include build time environment variables
-. "$PSScriptRoot\build_env.ps1"
+Import-EnvFile -EnvFilePath "$PSScriptRoot\build.env"
 
 if ( "$env:REPOSITORY" -eq "" || "$env:IMAGE_NAME" -eq "" ) {
-    Write-Error "REPOSITORY or IMAGE_NAME not set in build_env.ps1!"
+    Write-Error "REPOSITORY or IMAGE_NAME not set in build.env!"
     exit 1
 }
 
 # Determine architecture-specific tag
-if ( "$env:PROCESSOR_ARCHITECTURE" -eq "AMD64" ) {
-    $tag = "latest"
-} elseif ( "$env:PROCESSOR_ARCHITECTURE" -eq "ARM64" ) {
-    $tag = "arm64"
+if ( "$Architecture" -eq "amd64" ) {
+    $Tag = "latest"
+} elseif ( "$Architecture" -eq "arm64" ) {
+    $Tag = "arm64"
 } else {
-    Write-Error "Unsupported architecture: $env:PROCESSOR_ARCHITECTURE"
+    Write-Error "Unsupported architecture: $Architecture"
     exit 1
 }
 
@@ -39,10 +72,10 @@ if (Get-Command podman -ErrorAction SilentlyContinue) {
     exit 1
 }
 
-$imageName = "${env:REPOSITORY}/${env:IMAGE_NAME}:$tag"
+$imageName = "${env:REPOSITORY}/${env:IMAGE_NAME}:$Tag"
 Write-Verbose "Building container image: $imageName"
 
-& $containerTool build -t "$imageName" .
+& $containerTool build --platform linux/$Architecture -t "$imageName" .
 if ($?) {
     Write-Host "Container image built successfully."
 } else {
